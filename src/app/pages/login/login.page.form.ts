f71a50383372d9f092dfdec7542be2f25425fbe4
Rafał Nowak <rwojciechnowak@gmail.com>
2023-04-11 11:50:04 +0200

    feat: basic login form validation

---
+++ src/app/pages/login/login.page.form.ts
@@ -0,0 +1,14 @@
+import { FormBuilder, FormGroup } from "@angular/forms";
+
+export class LoginPageForm {
+
+    private formBuilder: FormBuilder; 
+
+    constructor(formBuilder: FormBuilder){
+        this.formBuilder = FormBuilder;
+    }
+
+    createForm() : FormGroup{
+        return null;
+    }
+}
