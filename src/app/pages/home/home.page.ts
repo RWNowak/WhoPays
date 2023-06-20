4e3f70034161815f22162dd1f6652b2bbf495526
Rafał Nowak <rwojciechnowak@gmail.com>
2023-06-20 11:05:31 +0200

    feat: upload profile picture

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { PaysComponent } from './pays.component'
+import { AuthService, UserPro } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  user: UserPro = { name: '', uid: '', email: '', photoUrl: ''};
+
   component = PaysComponent;
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    const uid = this.authService.getUID();
+    this.authService.getUserData(uid).then((userData) => {
+      this.user.name = userData.name;
+      this.user.email = userData.email;
+      this.user.photoUrl = userData.photoUrl; 
+    }).catch((error) => {
+      console.error(error);
+    });
   }
-
 }
